fc9ad246125127456a880d103fe3176b82142d12
rwancosta <rwan.daris@gmail.com>
2018-03-28 17:39:53 -0300

    Criação do cabeçalho.php

---
+++ cabecalho.php
@@ -0,0 +1,15 @@
+<header>
+	<meta charset="utf-8"/>
+	<title>Exercicio_1</title>
+</header>
+
+<body>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="cadastro_usuario.php">Usuários</a></li>
+		<li><a href="calculo_de_imc.php">IMC</a></li>
+		<li><a href="calculadora.php">Calculadora</a></li>
+	</ul>
+</body>
+			
+				
