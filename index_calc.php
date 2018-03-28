5e1cd96dca195e215384f6c9515fe0426bec421f
DaleteSamara <Daalete2@hotmail.com>
2018-03-28 17:25:02 -0300

    Parte do imc

---
+++ index_calc.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML> 
+<html>
+	
+	<form>
+		<body>
+			 <label>Digite o seu peso:</label>
+			 <input type="number" name="peso" >
+			 
+			 <label>Digite sua altura:</label>
+			 <input type="number" name="alt" >
+			
+			<input type="submit" value="Enviar">
+		  </form>	 
+		</body>
+</html>
