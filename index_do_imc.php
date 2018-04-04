f13b697c4ab36c1a8b67c881d0c2ec7c1d4766fe
DaleteSamara <Daalete2@hotmail.com>
2018-04-04 17:29:46 -0300

    Formulário do Imc

---
+++ index_do_imc.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML> 
+<html>
+	
+	<form method="POST" action="registra_imc.php">
+		<body>
+		
+		     <label>Digite seu nome:</label>
+			 <input type="text" name="nome"/>
+			 <label>Digite o seu peso:</label>
+			 <input type="number" name="peso" >
+			 
+			 <label>Digite sua altura:</label>
+			 <input type="number" name="alt" >
+			
+			<input type="submit" value="Enviar">
+		  </form>	 
+		</body>
+</html>
